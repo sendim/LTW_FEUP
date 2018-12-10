0e5bd0282c3695545336cdd3a70448e60c3ca786
sendim <henrique000@live.com.pt>
2018-12-10 14:21:12 +0000

    Added search page

---
+++ pages/search.php
@@ -0,0 +1,22 @@
+<?php
+  include_once('../database/db_search.php');
+  include_once('../templates/layout.php');
+  include_once('../templates/search.php');
+ 
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['error_messages'][] = "Login Required!";
+    die(header('Location: login.php'));
+  }
+
+
+  drawLayout(function(){
+
+    $input = $_SESSION['search'];
+    $profiles = searchProfiles($input);
+    $stories = searchStory($input);
+    $comments = searchComments($input);
+    $channels = searchChannels($input);
+    drawSearchFeed($profiles, $stories, $comments, $channels);
+  }, 'search');
+  
+?>
