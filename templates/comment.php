1b15c49b7ca62fb93765c89cccc694a70bca3f1e
sendim <henrique000@live.com.pt>
2018-12-10 15:46:42 +0000

    Easier to display when searching

--- templates/comment.php
+++ templates/comment.php
@@ -2,6 +2,20 @@
 		include_once('../database/db_user.php');
 		include_once('../database/db_comment.php');
 
+		function drawSimpleComment($comment) {
+			$username = getUserUsername($comment['userId']);
+			$text = $comment['text'];
+			?>
+				<div class="comment story-card bg-white" id="<?=$comment['commentId']?>">   
+					<p>
+						<a href="profile.php?username=<?=$username?>">@<?=$username?></a>
+						<?= ' ' . $text ?>
+					</p>
+				
+			<?php 
+		}
+
+
 		function drawComment($comment) {
 		/**
 		 * Draws the comment section.
@@ -9,16 +23,11 @@
 				$username = getUserUsername($comment['userId']);
 				$text = $comment['text'];
 				$subComments = getCommentsOfComment($comment['commentId']);
-		?>
-				<div class="comment story-card bg-white" id="<?=$comment['commentId']?>">   
-						<p>
-								<a href="profile.php?username=<?=$username?>">@<?=$username?></a>
-								<?= ' ' . $text ?>
-						</p>
-								
-						<hr/>
 
-						<footer>
+				drawSimpleComment($comment)
+		?>  									
+						<hr/>
+ 						<footer>
 								<div class="story-footer-left">
 										<div class="vote-buttons">
 												<a class="button primary icon" commentId="<?=$comment['commentId']?>" username="<?=$_SESSION['username']?>" vote="1" csrf="<?=$_SESSION['csrf']?>">
@@ -39,12 +48,12 @@
 										<a href="#<?=$comment['commentId']?>" csrf="<?=$_SESSION['csrf']?>"><?='Reply(' . sizeof($subComments) . ')'?></a>
 								</div>
 						</footer>
-
-						<div class="replies">
+ 						<div class="replies">
 								<?php foreach($subComments as $subComment)
 										drawComment($subComment);
 								?>
 						</div>
+ 				</div>
+<?php 
 
-				</div>
-<?php } ?>
+} ?>
