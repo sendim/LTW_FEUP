a7b153ba027874d9eade1d6efad0c53275bc8e2a
sendim <henrique000@live.com.pt>
2018-12-10 17:26:06 +0000

    Update search.php

--- templates/search.php
+++ templates/search.php
@@ -3,24 +3,14 @@
     include_once('../templates/profile.php');
     include_once('../templates/channel.php');
 
-    function drawSearchFeed($profiles, $stories, $comments, $channels) { ?>
+    function drawSearchFeed($profiles, $stories, $comments, $channels, $input) { ?>
         <div id="feed">
 
             <section class="container bg-white">
 
                 <header>
-                    <h2>Search Results:</h2>
+                    <h2>Search Results: <?php  echo $input ?> </h2>
                 </header>
-
-                <hr/>
-
-                <a class="button secondary" href="newStory.php">
-                    New story
-                </a>
-
-                <button class="button secondary">Select channel</button>
-                <button class="button secondary">Order</button> 
-
             </section>
 
             <div>
@@ -28,36 +18,63 @@
                     <h2>Profiles:</h2>
                 </header>
                 <?php
-                    foreach($profiles as $profile)
-                        drawHeaderProfile($profile);
-                ?>
+                    if(count($profiles) != 0){
+                        foreach($profiles as $profile)
+                            drawHeaderProfile($profile);
+                    }
+                    else {
+                        ?>
+                        No results found.
+                        <?php 
+                    } ?> 
                 <header>
                     <h2>Stories:</h2>
                 </header>
                 <?php
-                    foreach($stories as $story)
-                        drawStory($story);
-                ?>
+                    if(count($stories) != 0){
+                        foreach($stories as $story)
+                            drawStory($story); 
+                    }
+                    else {
+                        ?>
+                        No results found.
+                        <?php 
+                    } ?> 
                 <header>
                     <h2>Comments:</h2>
                 </header>
                 <?php
-                    foreach($comments as $comment) {
-                        drawSimpleComment($comment);
+                    if(count($comments) != 0){
+                        foreach($comments as $comment) {
+                            drawSimpleComment($comment);
+                            ?>
+                            </div> 
+                        <?php 
+                        } 
+                    }
+                    else {
                         ?>
-                        </div> 
-                    <?php } ?>
-                        
+                        No results found.
+                        <?php 
+                    } ?> 
                 
                 <header>
                     <h2>Channels:</h2>
                 </header>
-                
-                <section id="channels">
-                    <?php
-                    foreach($channels as $channel)
-                        drawChannelCard($channel);
-                    ?>
+                <?php
+                    if(count($channels) != 0){
+                        ?>
+                        <section id="channels">
+                        <?php 
+                            foreach($channels as $channel)
+                                drawChannelCard($channel);
+                            
+                        }
+                    else {
+                        ?>
+                        No results found.
+                        <?php 
+                    } ?> 
                 </section>
             </div>
 
